deb4d9f0e78b8f50874211ed25c13a80c50b3d24
Administrator <Administrator@192.168.0.29>
2018-06-14 11:28:27 +0530

    TL initial commit

---
+++ WishApp/src/com/pp/beans/WishMessageGenerator.java
@@ -0,0 +1,27 @@
+package com.pp.beans;
+
+import java.util.Date;
+
+public class WishMessageGenerator {
+	private Date date;
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	
+	public String generateWishMessage(String user) {
+		int hour =date.getHours();
+		if(hour<12) {
+			return "Good Morning "+user;
+		}
+		else if(hour<16) {
+			return "Good Afternoon "+user;
+		}
+		else if(hour<20) {
+			return "Good Evening "+user;
+		}
+		else {
+			return "Good Night "+user;
+		}
+	}
+}
---
+++ WishApp/src/com/pp/test/SetterInjectionTest.java
@@ -0,0 +1,23 @@
+package com.pp.test;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+
+import com.pp.beans.WishMessageGenerator;
+
+public class SetterInjectionTest {
+
+	public static void main(String[] args) {
+		Resource res=null;
+		BeanFactory factory=null;
+		WishMessageGenerator wish=null;
+		res=new FileSystemResource("src/com/pp/cfgs/applicationContext.xml");
+		//create IOC Container (BeanFactory)
+        factory=new XmlBeanFactory(res);
+      wish=(WishMessageGenerator)factory.getBean("wish");
+     System.out.println("Wish Message : "+wish.generateWishMessage("Ram")); 
+	}
+
+}
